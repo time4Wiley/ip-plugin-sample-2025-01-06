8e9f8a18fdb156cb510d1556847211269ee3996a
GitHub Action <action@github.com>
2025-01-06 11:55:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/time4wiley/ippluginsample20250106/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/time4wiley/ippluginsample20250106/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.time4wiley.ippluginsample20250106.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.time4wiley.ippluginsample20250106.MyBundle
+import com.github.time4wiley.ippluginsample20250106.services.MyProjectService
 import javax.swing.JButton
 
 
